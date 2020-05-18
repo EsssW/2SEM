aa653396ceb094d9ed687eb7778cbe316c09493d
EsssW <43808999+EsssW@users.noreply.github.com>
2020-05-18 12:36:34 +0300

    Add files via upload

---
+++ Matrix.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix_class_28._04._2020
+{
+    class Matrix
+    {
+        int m, n;
+        double[,] a;
+        
+        public Matrix() { a = null; m = 0; n = 0; }
+        public Matrix(int mm, int nn)
+        {
+            m = mm; n = nn;
+            a = new double[m, n];
+        }
+
+        public int M { get { return m; } }
+        public int N { get { return n; } }
+
+        public double this[int i, int j]
+        {
+            get
+            {
+                if (i < 0 || i >= m || j < 0 || j >= n) throw new Exception("Bad index!");
+                return a[i, j];
+            }
+            set
+            {
+                if (i < 0 || i >= m || j < 0 || j >= n) throw new Exception("Bad index!");
+                a[i, j] = value;
+            }
+
+        }
+
+        public static Matrix operator ! (Matrix x)
+        {
+            Matrix res = new Matrix(x.n, x.m);
+            for (int i = 0; i < x.m; i++)
+            {
+                for (int j = 0; j < x.n; j++)
+                {
+                    res[j, i]= x.a[i, j];
+                }
+            }
+            return res;
+        }
+
+        public static Matrix operator * (Matrix x, double k) 
+        {
+            Matrix res = new Matrix(x.m, x.n);
+            for (int i = 0; i < x.m; i++)
+            {
+                for (int j = 0; j < x.n; j++)
+                {
+                    res[i, j] = x.a[i, j] * k;
+                }
+            }
+            return res;
+        }
+
+        public static Matrix operator *(double k, Matrix x) 
+        {
+            Matrix res = new Matrix(x.m, x.n);
+            for (int i = 0; i < x.m; i++)
+            {
+                for (int j = 0; j < x.n; j++)
+                {
+                    res[i, j] = x.a[i, j] * k;
+                }
+            }
+            return res;
+        }
+
+        public static Matrix operator +(Matrix x, Matrix y)
+        {
+            if ( x.m!=y.m || x.n!=y.n) throw new Exception("размеры матриц не совпадают");
+            Matrix res = new Matrix(x.m, x.n);
+            for (int i = 0; i < x.m; i++)
+            {
+                for (int j = 0; j < x.n; j++)
+                {
+                    res[i, j] = x.a[i, j] + y.a[i,j];
+                }
+            }
+            return res;
+        }
+
+        public static Matrix operator -(Matrix x, Matrix y)
+        {
+            if (x.m != y.m || x.n != y.n) throw new Exception("размеры матриц не совпадают");
+            Matrix res = new Matrix(x.m, x.n);
+            for (int i = 0; i < x.m; i++)
+            {
+                for (int j = 0; j < x.n; j++)
+                {
+                    res[i, j] = x.a[i, j] - y.a[i, j];
+                }
+            }
+            return res;
+        }
+
+        public static Matrix operator -(Matrix x) 
+        {
+            Matrix res = new Matrix(x.m, x.n);
+            for (int i = 0; i < x.m; i++)
+            {
+                for (int j = 0; j < x.n; j++)
+                {
+                    res[i, j] = x.a[i, j] * -1;
+                }
+            }
+            return res;
+        }
+
+        public static Matrix operator *(Matrix x, Matrix y) 
+        {
+            if (x.n != y.m) throw new Exception("Матрицы нельзя перемножить");
+            Matrix res = new Matrix(x.m, y.n);
+            for (int i = 0; i < x.m; i++)
+            {
+                for (int j = 0; j < y.n; j++)
+                {
+                   res[i, j] += x.a[i, j] * y.a[i, j];            
+                }
+            }
+            return res;
+        }
+
+        public static Matrix operator ++(Matrix x)
+        {
+            Matrix res = new Matrix(x.m, x.n);
+            for (int i = 0; i < x.m; i++)
+            {
+                for (int j = 0; j < x.n; j++)
+                {
+                    res[i, j] = x.a[i, j] + 1;
+                    
+                }
+            }
+            return res;
+        }
+
+        public static Matrix operator --(Matrix x) 
+        {
+            Matrix res = new Matrix(x.m, x.n);
+            for (int i = 0; i < x.m; i++)
+            {
+                for (int j = 0; j < x.n; j++)
+                {
+                  res[i, j] = x.a[i, j] - 1;
+                }
+            }
+            return res;
+        }
+        /*
+        public static bool operator == (Matrix x, Matrix y)
+        {
+            bool f = true ;
+            if (x.m != y.m || x.n != y.n) { return false; }
+            for (int i = 0; i < x.m; i++)
+            {
+                for (int j = 0; j < x.n; j++)
+                {
+                    if (x.a[i, j] != y.a[i, j]) f = false;
+                }
+            }
+            return f;
+        }
+
+        public static bool operator != (Matrix x, Matrix y)
+        {
+            bool f = false;
+            if (x.m != y.m || x.n != y.n) { return true; }
+            for (int i = 0; i < x.m; i++)
+            {
+                for (int j = 0; j < x.n; j++)
+                {
+                    if (x.a[i, j] != y.a[i, j]) f = true;
+                }
+            }
+            return f;
+        }
+        */
+
+        public static explicit operator double[,](Matrix x)
+
+        {
+
+            double[,] res = new double[x.m, x.n];
+
+            for (int i = 0; i < x.m; i++)
+
+                for (int j = 0; j < x.n; j++)
+
+                    res[i, j] = x.a[i, j];
+
+            return res;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < m; i++, Console.WriteLine())
+                for (int j = 0; j < n; j++)
+                    Console.Write("{0, 6}", a[i, j]);
+            Console.WriteLine();
+
+        }
+
+        public void WriteMat()
+        {
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.WriteLine("Введите элемент матрицы {0}:{1}", i + 1, j + 1);
+                    a[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+        }
+
+        public bool OneMatrix()
+        {
+            int count = 0;
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                    if (a[i, j] == 1 && i == j) { count++; }
+
+            if (count == n) { return true; }
+            else { return false; }
+        }
+    }
+
+
+
+
+}
---
+++ squareMatrix.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix_class_28._04._2020
+{
+    class squareMatrix : Matrix
+    {
+        public squareMatrix(int n) :base(n, n) { }
+        public squareMatrix() { }
+
+        // симметричность матрицы 
+        public  bool SemetricMatrix()
+        {
+            bool f = true;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[i, j] != a[j, i])
+                    {
+                        f= false;
+                        break;
+                    }
+                }
+                if (!f) break;
+            }
+            if (f) return true;
+            else return false;
+        }
+            
+        // Определтиьель 
+        public double Determinant()
+        {
+            double det = 0;
+            int rows = n;
+            if (rows == 1)
+                return a[0, 0];
+            squareMatrix na = new squareMatrix(rows - 1);
+            for (int j = 0; j < rows; j++)
+            {
+                na = SubMatrix(0, j);
+                if (j % 2 == 0)
+                    det += na.Determinant() * a[0, j];
+                else
+                    det -= na.Determinant() * a[0, j];
+            }
+            return det;
+        }
+        //обртная матрица
+        public squareMatrix Obratnya(squareMatrix m)
+        {
+            int rows = n;
+            squareMatrix res = new squareMatrix(rows);
+            squareMatrix na = new squareMatrix(rows - 1);
+            double det =m.Determinant();
+            if (det == 0)
+                throw new Exception("Матрица вырожденная");
+            int k;
+            for (int i = 0; i < rows; i++)
+            {
+                if (i % 2 == 0) k = 1;
+                else k = -1;
+                for (int j = 0; j < rows; j++)
+                {
+                    na = m.SubMatrix(i, j);
+                    res[j, i] = k * na.Determinant() / det;
+                    k = -k;
+                }
+            }
+            return res;
+        }
+        // минор  n-того порядка
+        squareMatrix SubMatrix(int i1, int j1)
+        {
+            int rows = n;
+            squareMatrix na = new squareMatrix(rows - 1);
+            for (int i = 0; i < i1; i++)
+            {
+                for (int j = 0; j < j1; j++)
+                    na[i, j] = a[i, j];
+                for (int j = j1 + 1; j < rows; j++)
+                    na[i, j - 1] = a[i, j];
+            }
+            for (int i = i1 + 1; i < rows; i++)
+            {
+                for (int j = 0; j < j1; j++)
+                    na[i - 1, j] = a[i, j];
+                for (int j = j1 + 1; j < rows; j++)
+                    na[i - 1, j - 1] = a[i, j];
+            }
+            return na;
+        }
+
+
+
+    }
+}
