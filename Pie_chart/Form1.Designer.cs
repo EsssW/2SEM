dd9e46f2676e81d3195f79606ea408585d45d20b
EsssW <43808999+EsssW@users.noreply.github.com>
2020-05-22 15:18:04 +0300

    Add files via upload

---
+++ Pie_chart/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Mullinov_Aidar_09_901
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Chart1.Legends.Add(legend1);
+            this.Chart1.Location = new System.Drawing.Point(16, 11);
+            this.Chart1.Name = "Chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.Chart1.Series.Add(series1);
+            this.Chart1.Size = new System.Drawing.Size(771, 429);
+            this.Chart1.TabIndex = 0;
+            this.Chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Chart1);
+            this.Name = "Form1";
+            this.Text = "eujde. ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart1;
+    }
+}
+
---
+++ Pie_chart/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Mullinov_Aidar_09_901
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Chart1.Series.Clear();
+            // настройки диаграммы
+            Chart1.BackColor = Color.Gray;
+            Chart1.BackSecondaryColor = Color.WhiteSmoke;
+            Chart1.BackGradientStyle = GradientStyle.DiagonalRight;
+            Chart1.BorderlineDashStyle = ChartDashStyle.Solid;
+            Chart1.BorderlineColor = Color.Gray;
+            Chart1.BorderSkin.SkinStyle = BorderSkinStyle.Emboss;
+            Chart1.ChartAreas[0].BackColor = Color.Wheat;
+            // заголовок
+            Chart1.Titles.Add("Диаграмма");
+            //стиль и размер заголвка 
+            Chart1.Titles[0].Font = new Font("", 16);
+            
+            Chart1.Series.Add(new Series("Result"){ChartType = SeriesChartType.Pie});
+            int[] arr;// массив для записи данных из файла
+           // запоняем массив при помощи вызова метода
+            arr = ReadFromFile("C:\\Users\\mukht\\Desktop\\ocenki.txt");
+            // переменные для подсчета количесвто оценок 
+            int a5=0, a4=0, a3=0, a2=0;
+            // цикл для подсчета числа оценок
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int casei = arr[i];
+                switch (casei)
+                {
+                    case 5: a5 += 1; break;
+                    case 4: a4 += 1; break;
+                    case 3: a3 += 1; break;
+                    case 2: a2 += 1; break;
+                    default: MessageBox.Show("В файле на строке {0} неверная оценка ");  break;
+                }
+            }
+            // задаем данные для диаграммы
+            double[] yValues = { a5, a4, a3, a2 };
+            string[] xValues = { "Отл", "Хор", "Удов", "НеУдов" };
+            Chart1.Series["Result"].Points.DataBindXY(xValues, yValues);
+            // 3D стиль для диаграммы
+            Chart1.ChartAreas[0].Area3DStyle.Enable3D = true;
+        }
+        // Метод для создания массива из файла
+        public int[] ReadFromFile(string FileName)
+        {
+            // считываем файл
+            StreamReader f = new StreamReader (FileName);
+            string line; // промежуточная строка
+            int count= System.IO.File.ReadAllLines(FileName).Length; // вычиляем кол-во строк файла
+            int[] arr = new int[count]; // массив для хранения всех элементов файла 
+            int i = 0;
+            // в цикле записывем каждую строку в отдельную ячейку массива
+            while ((line = f.ReadLine()) != null)
+            {
+                arr[i++] = Convert.ToInt32(line);
+            }
+            return arr;
+        }
+    }
+}
