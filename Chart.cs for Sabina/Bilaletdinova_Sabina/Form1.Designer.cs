61c55c2876bd47a912804467035d58dec29dafc8
EsssW <43808999+EsssW@users.noreply.github.com>
2020-05-22 14:06:01 +0300

    Add files via upload

---
+++ Chart.cs for Sabina/Bilaletdinova_Sabina/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Bilaletdinova_Sabina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(12, 11);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(1121, 579);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 596);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сетка по X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(212, 596);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 60);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Сетка по Y";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 668);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Chart.cs for Sabina/Bilaletdinova_Sabina/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace Bilaletdinova_Sabina
+{
+    public partial class Form1 : Form
+    {
+        static bool fn_enable = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Метод для создания массива из файла
+        public double[,] ReadFromFile(string FileName)
+        {
+            int rows = 0;
+            string tmp;
+            char[] sep = { ' ' };
+            FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            while (!sr.EndOfStream)
+            {
+                sr.ReadLine();
+                rows++;
+            }
+            double[,] array = new double[rows, 2];
+            fs.Seek(0, SeekOrigin.Begin);
+            int i = 0;
+            while (!sr.EndOfStream)
+            {
+                tmp = sr.ReadLine();
+                string[] s = tmp.Split(sep, StringSplitOptions.RemoveEmptyEntries);
+                array[i, 0] = double.Parse(s[0]);
+                array[i, 1] = double.Parse(s[1]);
+                i++;
+
+            }
+            sr.Close();
+            fs.Close();
+            return array;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series.Clear(); //Очистка всех графиков
+            chart1.Series.Add(new Series("МАЙ")); //  Создание ноовго графика
+            // указываем диапозон 
+            chart1.ChartAreas[0].AxisX.Minimum = 1; 
+            chart1.ChartAreas[0].AxisX.Maximum = 31;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = 33;
+            // задаем название осям
+            chart1.ChartAreas[0].AxisX.Title = "День";
+            chart1.ChartAreas[0].AxisY.Title = "Температура";
+            //задаем шаг(интрвал осей)
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            chart1.ChartAreas[0].AxisY.Interval = 1;
+            // указываем позицию названий осей для правильного отображения 
+            chart1.ChartAreas[0].AxisX.TitleAlignment = StringAlignment.Near;
+            chart1.ChartAreas[0].AxisY.TitleAlignment = StringAlignment.Far;
+            // задаем цвет
+            chart1.Series[0].Color = Color.Green;
+            // создаем массив для заполнения из файла
+            double[,] arr;
+            // созданому массиву вызываем метод для читки и заолнения массива из файла
+            arr = ReadFromFile("C:\\Users\\mukht\\Desktop\\weather.txt");
+            // циклы для вывода наших данных из массива
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    chart1.Series[0].Points.AddXY(i+1, arr[i, j]);
+                }
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (fn_enable == true)
+            {
+                chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = true;
+                fn_enable = false;
+            }
+            else
+            {
+                chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+                fn_enable = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (fn_enable == true)
+            {
+                chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = true;
+                fn_enable = false;
+            }
+            else
+            {
+                chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+                fn_enable = true;
+            }
+        }
+    }
+}
