73fe3c230ff92bbb39f21ef8fa17720d1271a83c
EsssW <43808999+EsssW@users.noreply.github.com>
2020-06-04 10:27:49 +0300

    Add files via upload

---
+++ MARCHENKO_T.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+class NameFon
+{
+    protected string name;
+    protected string fon;
+
+    public NameFon() { name = ""; fon = ""; }
+    public NameFon(string n,string f) { name = n; fon = f; }
+
+    public string N
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public string F
+    {
+        get { return fon; }
+        set { fon = value; }
+    }
+    public void Display()
+    {
+        Console.WriteLine("Name= "+name+ " Fon= "+fon);
+    }
+
+    public static bool operator < (NameFon a, NameFon b)
+    {
+        bool f = false;
+        if (a.name[0] < b.name[0]) f = true;
+        return f;
+    }
+    public static bool operator >(NameFon a, NameFon b)
+    {
+        bool f = false;
+        if (a.name[0] > b.name[0]) f = true;
+        return f;
+    }
+}
+
+class GuideFon
+{
+    NameFon[] guide;
+    int size;
+
+    public GuideFon() { size = 0; guide = new NameFon[0]; }
+    public GuideFon(NameFon[] arr, int n)
+    {
+        guide = arr;
+        size = n;
+    }
+    public void Display()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine(guide[i].F+" " +guide[i].N);
+        }
+    }
+    public bool Find(string name)
+    {
+        bool f = false;
+        for (int i = 0; i < size; i++)
+        {
+            if (guide[i].N == name) f = true;
+        }
+        return f;
+    }
+    public static GuideFon operator +(GuideFon guidf, NameFon namef)
+    {
+        NameFon[] arr = new NameFon[guidf.guide.Length + 1];
+        for (int i = 0; i < guidf.guide.Length; i++)
+        {
+            arr[i] = guidf.guide[i];
+        }
+        arr[guidf.guide.Length] = namef;
+        return new GuideFon(arr, guidf.size + 1);
+    }
+    public void Sort()
+    {
+        for (int j = 0; j < guide.Length; j++)
+        {
+            for (int i = 0; i < guide.Length; i++)
+            {
+                if (i < (guide.Length - 1))
+                {
+                    if (guide[i + 1] < guide[i])
+                    {
+                        NameFon change = guide[i + 1];
+                        guide[i + 1] = guide[i];
+                        guide[i] = change;
+                    }
+                }
+            }
+        }
+    }
+}
+
+class Demo
+{
+    public static void Main()
+    {
+        GuideFon gf=new GuideFon();
+        // создание абонентов
+        NameFon n1 = new NameFon("Марченко","8999221221");
+        NameFon n2 = new NameFon("Иванов", "89991767621");
+        NameFon n3 = new NameFon("Сидоров", "89991765433");
+        // создания массива абонентов для упрощения работы
+        NameFon[] arr = { n1, n2, n3 };
+        //заролнение справочника
+        for (int i = 0; i < arr.Length; i++)
+        {
+            gf = gf + arr[i];
+        }
+        // вывод на экран
+        gf.Display();
+        Console.WriteLine("===============");
+        // проверка по справочнику
+        string namefind = "Иванов";
+        if (gf.Find(namefind)) Console.WriteLine("{0} Есть в справочнике", namefind);
+        else Console.WriteLine("{0} НЕТ в справочнике", namefind);
+        // сортировка и вывод
+        Console.WriteLine("===============");
+        Console.WriteLine("После сортировки");
+        gf.Sort();
+        gf.Display();
+    }
+}
---
+++ Subbotina_A.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+
+class Group
+{
+    public int n;
+    public string[] group;
+    //свойство
+    public int N
+    {
+        get { return n; }
+        set { n = value; }
+    }
+    //Конструктор
+    public Group(string[] g)
+    {
+        n = g.Length;
+        group = g;
+    }
+    //индексатор
+    public string this[int index]
+    {
+        get
+        { 
+            return group[index];
+        }
+        set 
+        {
+            group[index] = value;
+        }
+    }
+    // метод копирования
+    public static void Copy (Group group1, Group group2)
+    {
+        group2.group = group1.group;
+        group2.n = group1.n;
+    }
+    //мето для сортировки
+    public void Sort()
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (i < (n - 1))
+                {
+                    if (group[i + 1][0] < group[i][0])
+                    {
+                        string str = group[i + 1];
+                        group[i + 1] = group[i];
+                        group[i] = str;
+                    }
+                }
+            }
+        }
+    }
+    // метод для вывода в консоль
+    public virtual void Display()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"{i+1}" + group[i]+" ");
+        }
+    }
+    // метод для поиска фамилии в списке
+    public bool Find(string name)
+    {
+        bool b = false;
+        for (int i = 0; i < n; i++)
+        {
+            if (group[i] == name) b = true;
+        }
+        return b;
+    }
+    // перегрезка оператора для удаления фамилии из списка
+    public static Group operator -(Group group1, string newname)
+    {
+        string[] strarr = new string[group1.n - 1];
+        if (group1.Find(newname))
+        {
+            for (int i = 0; i < group1.n; i++)
+                if (newname == group1.group[i])
+                    group1.group[i] = "";
+            
+            for (int i = 0; i < group1.n; i++)
+            {
+                if (group1.group[i] != "")
+                {
+                    strarr[i] = group1.group[i];
+                }
+                else
+                {
+                    strarr[i] = group1.group[i + 1];
+                    i++;
+                }
+            }
+        }
+        return new Group(strarr);
+    }
+    //метод дял записи в файл
+    public virtual void InFile(string path)
+    {
+        StreamWriter sw = new StreamWriter(path);
+        for (int i = 0; i < n; i++)
+        {
+            sw.WriteLine( $"{i+1}) " + group[i]);
+        }
+        sw.Close();
+    }
+
+}
+class StudGroup  : Group
+{
+    string number;
+    string facultet;
+    public StudGroup(string number,string facultet,string[] group) :base(group)
+    {
+        this.number = number;
+        this.facultet = facultet;
+    }
+    public override void Display()
+    {
+        Console.WriteLine($"Факультет {facultet}  Группа {number}");
+        base.Display();
+    }
+    public override void InFile(string path)
+    {
+        StreamWriter sw = new StreamWriter(path);
+        for (int i = 0; i < n; i++)
+        {
+            sw.WriteLine((i + 1) + ") " + group[i] + ", " + number + ", " + facultet);
+        }
+        sw.Close();
+    }
+    
+}
+class Demo
+{
+    public static void Main()
+    {
+        string[] name =
+        {
+            "Ивнов","Петров","Сидоров","Субботина","Алексеев"
+        };
+        Group gr = new Group(name);
+        StudGroup StudGrup = new StudGroup( "09-901", "ИВМИиТ", gr.group);
+        Console.WriteLine("##### ДО СОРТИРОВКИ #####");
+        StudGrup.Display();
+
+        Console.WriteLine("##### ПОСЛЕ СОРТИРОВКИ #####");
+        StudGrup.Sort();
+        StudGrup.Display();
+        string n = "Сидоров";
+        Console.WriteLine("##### После удаления ={0}= #####", n);
+       
+        gr = gr- n;//удаляем из масиива grup класса group
+        StudGrup = new StudGroup("09-901", "ИВМИиТ", gr.group); // записываем заново удалив 
+        StudGrup.Display();
+        StudGrup.InFile("..\\..\\Text.txt");
+
+    }
+}
